eb53773ba5560ebfc054f1236e1f4e7f560e05aa
Yogesh Kale <65170420+Yogesh3042@users.noreply.github.com>
2021-08-13 15:22:02 +0530

    First Commit

---
+++ main/main.go
@@ -0,0 +1,10 @@
+package main
+
+import(
+	"fmt"
+	"mypkg"
+)
+func main(){
+	res:=mypkg.Hi()
+	fmt.Println(res)
+}
---
+++ mypkg/mypkg.go
@@ -0,0 +1,7 @@
+package mypkg
+import "fmt"
+
+func Hi() string{
+	fmt.Println("I am accessing this hi package ")
+  return "Hello package in checked"
+}
